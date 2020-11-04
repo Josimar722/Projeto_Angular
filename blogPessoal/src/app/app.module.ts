0aad25216db8942e3e27b38c14258e66eaff85e1
unknown <josimarsccp@gmail.com>
2020-11-04 19:42:43 -0300

    consumo de api

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +11,9 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { FeedComponent } from './feed/feed.component';
 import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -16,11 +22,16 @@ import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.componen
     NavbarComponent,
     FooterComponent,
     FeedComponent,
-    PerfilLateralComponent
+    PerfilLateralComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ blogPessoal/src/app/model/Postagem.ts
@@ -0,0 +1,10 @@
+import { Data } from '@angular/router'
+import { Tema } from './Tema'
+
+export class Postagem{
+    public id: number
+    public titulo: string
+    public texto: string
+    public data: Data
+    public tema: Tema
+}
